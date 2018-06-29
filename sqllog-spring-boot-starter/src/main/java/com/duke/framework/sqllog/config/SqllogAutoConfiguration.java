2b53f35d2e35cb6b2ca1e0ccf4588e36019dfc91
duke <duke0417@foxmail.com>
2018-06-29 18:28:33 +0800

    解决sqllog 查询的时候不打印sql的问题

--- sqllog-spring-boot-starter/src/main/java/com/duke/framework/sqllog/config/SqllogAutoConfiguration.java
+++ sqllog-spring-boot-starter/src/main/java/com/duke/framework/sqllog/config/SqllogAutoConfiguration.java
@@ -1,12 +1,10 @@
 package com.duke.framework.sqllog.config;
 
-import com.duke.framework.sqllog.SqllogProperties;
 import com.duke.framework.sqllog.interceptor.MybatisSqlLogInterceptor;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -16,12 +14,15 @@ import java.util.List;
  * Created duke on 2018/6/28
  */
 @Configuration
-@ConditionalOnProperty(value = "duke.sqllog.enabled", matchIfMissing = true)
+@ConditionalOnProperty(value = "duke.sqllog.enabled", havingValue = "true", matchIfMissing = true)
 public class SqllogAutoConfiguration {
 
     @Autowired
     private List<SqlSessionFactory> sqlSessionFactories;
 
+    /**
+     * 这里因为在addMybatisSqlLogInterceptor之前是需要SqlSessionFactory依赖进来的，所以要使用@PostConstruct注解
+     */
     @PostConstruct
     public void addMybatisSqlLogInterceptor() {
         System.out.println("sqllog starter autoconfig");
--- sqllog-spring-boot-starter/src/main/java/com/duke/framework/sqllog/interceptor/MybatisSqlLogInterceptor.java
+++ sqllog-spring-boot-starter/src/main/java/com/duke/framework/sqllog/interceptor/MybatisSqlLogInterceptor.java
@@ -1,5 +1,6 @@
 package com.duke.framework.sqllog.interceptor;
 
+import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
@@ -26,6 +27,7 @@ import java.util.Properties;
  */
 @Intercepts({
         @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class}),
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
         @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
 public class MybatisSqlLogInterceptor implements Interceptor {
 
