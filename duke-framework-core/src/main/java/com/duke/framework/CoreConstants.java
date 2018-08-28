a227f46183ce731ca94b2ee5eea689306aa4c723
duke <duke0417@foxmail.com>
2018-08-28 08:58:28 +0800

    提一个公共的常量类

--- duke-framework-core/src/main/java/com/duke/framework/CoreConstants.java
+++ duke-framework-core/src/main/java/com/duke/framework/CoreConstants.java
@@ -5,14 +5,14 @@ package com.duke.framework;
  */
 public class CoreConstants {
 
-    private static final String ACCESS_TOKEN = "_d_access_token_";
+    public static final String ACCESS_TOKEN = "_d_access_token_";
 
-    private static final String REFRESH_TOKEN = "_d_refresh_token_";
+    public static final String REFRESH_TOKEN = "_d_refresh_token_";
 
-    private static final String LOGIN_NAME = "_d_login_name_";
+    public static final String LOGIN_NAME = "_d_login_name_";
 
-    private static final String USER_ID = "_d_user_id_";
+    public static final String USER_ID = "_d_user_id_";
 
-    private static final String AVATAR = "_d_avatar_";
+    public static final String AVATAR = "_d_avatar_";
 
 }
