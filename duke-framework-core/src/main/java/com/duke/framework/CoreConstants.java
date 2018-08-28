93fc366883aedd27f890c4710b3598315679b26e
duke <duke0417@foxmail.com>
2018-08-28 08:42:42 +0800

    2018-08-14

---
+++ duke-framework-core/src/main/java/com/duke/framework/CoreConstants.java
@@ -0,0 +1,18 @@
+package com.duke.framework;
+
+/**
+ * Created duke on 2018/8/28
+ */
+public class CoreConstants {
+
+    private static final String ACCESS_TOKEN = "_d_access_token_";
+
+    private static final String REFRESH_TOKEN = "_d_refresh_token_";
+
+    private static final String LOGIN_NAME = "_d_login_name_";
+
+    private static final String USER_ID = "_d_user_id_";
+
+    private static final String AVATAR = "_d_avatar_";
+
+}
